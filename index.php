6d68b1c54c6b5dfeca446fc42294397c04271410
ZionFr <zionsafreire@gmail.com>
2025-04-29 22:18:41 -0300

    cadastrar e login

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $u = new Usuario;
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Projeto Login</title>
+        <title>Login</title>
         <link rel="stylesheet" href="TelaLogin.css">
         <link rel="stylesheet" href="style.css">
     </head>
@@ -25,14 +25,22 @@ $u = new Usuario;
                 <div class="line3"></div>
             </div>
             <ul class="nav-list">
-                <li><a href="destaques.html">Destaques</a></li>
-                <li><a href="jogosPC.html">Jogos Pc</a></li>
-                <li><a href="jogosConsole.html">Jogos Console</a></li>
-                <li><a href="jogosMobile.html">Jogos Mobile</a></li>
+                <li><a href="sobre.html">Sobre</a></li>
+                <li class="dropdown"><a href="#"> Produtos </a>
+
+                    <div class="dropdown-menu">
+                        <a href="jogosPC.html">Jogos Pc</a>
+                        <a href="jogosConsole.html">Jogos Console</a>
+                        <a href="jogosMobile.html">Jogos Mobile</a>
+                    </div>
+
+                </li>
+                <li><a href="contato.html">Contato</a></li>
                 <li><a href="index.php">Entrar</a></li>
             </ul>
         </nav>
     </header>
+    <main>
     <div id="corpo-form">
     <h1>Entrar</h1>
     <form method="POST">
@@ -40,9 +48,55 @@ $u = new Usuario;
         <input type="password" name="senha" placeholder="Senha" maxlength="32">
         <input type="submit" placeholder="Acessar">
         <a href="cadastrar.php">Ainda não tem uma conta? <strong>Cadastre-se</strong></a>
-    </div>
     </form>
+    </div>
+    </main>
 
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <img src="./img/chuva_branca2.png">
+                <p>Contatos</p>
+                <div id="footer_social_media">
+                    <a href="https://github.com/ZionFr/Chuuva" class="footer-link" id="github">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+
+                </div>
+            </div>
+
+            <div id="footer_subscribe">
+                <h3>Suporte</h3>
+
+                <p>
+                    Para qualquer dúvidas adicionais quanto ao funcionamento de nossa plataforma mande-nos seu email para melhor contato.
+                </p>
+
+                <div id="input_group">
+                    <input type="email" id="email" placeholder="Digite seu e-mail">
+
+                    <button onclick="emailConfirmar()">
+                        <i class="fa-regular fa-envelope"></i>
+                        <script src="./js/footer.js"></script>
+                        
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169
+            2025 Chuuva. all rights reserved
+        </div>
+    </footer>
 
 
 <?php
@@ -92,8 +146,6 @@ if(isset($_POST['email']))
 
 
 ?>
-
-
 
     </body>
 </html>
